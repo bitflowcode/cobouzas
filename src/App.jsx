6ce7129428e61bdb06586153e2073d559e83fc49
Bitflow Code <soporte@decodeapp.net>
2025-10-07 00:28:10 +0200

    Añadir ScrollToTop para resetear posición del scroll al navegar entre páginas

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import ScrollToTop from './components/ScrollToTop'
 import Layout from './components/Layout'
 import Home from './pages/Home'
 import Implantologia from './pages/Implantologia'
@@ -21,6 +22,7 @@ import './App.css'
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
